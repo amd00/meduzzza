1998cc38d090f3f00f10f4767dece42b6af2e2f8
Andrey Dudakov <amd00@amd00-desktop.orion.org>
2013-11-25 21:13:22 +0400

    Process scanner added

---
+++ sources/clamavqtengine/src/procscanner.cpp
@@ -0,0 +1,111 @@
+
+/*
+ *  Copyright (C) 2013 Andrey Dudakov
+ *
+ *  Authors: Andrey "amd00" Dudakov
+ *
+ *  This file is part of clamavqtengine.
+ *
+ *  clamavqtengine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  clamavqtengine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with clamavqtengine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QRegExp>
+#include <QDir>
+#include <QTextStream>
+
+#include <clamav.h>
+
+#include "procscanner.h"
+
+namespace Meduzzza
+{
+
+	void ProcScanner::run()
+	{
+		checkPause();
+		if(Scanner::stopped())
+			return;
+		
+		QRegExp mem_addr_regex("^([0-9a-fA-F]+)-([0-9a-fA-F]+)\\sr");
+		QDir system_proc_dir("/proc");
+		QDir proc_dir(system_proc_dir.absoluteFilePath(QString::number(m_pid)));
+		QString maps_file_str(proc_dir.absoluteFilePath("maps"));
+		QString mem_file_str(proc_dir.absoluteFilePath("mem"));
+		QString proc_name = QFileInfo(QFile::symLinkTarget(proc_dir.absoluteFilePath("exe"))).fileName();
+		QFile maps_file(maps_file_str);
+		QFile mem_file(mem_file_str);
+
+		if(!maps_file.open(QIODevice::ReadOnly) || !mem_file.open(QIODevice::ReadOnly))
+		{
+			if(maps_file.error() != QFile::NoError)
+			{
+				qCritical("ERROR: Open file error: maps: %s", maps_file.errorString().toLocal8Bit().data());
+				Q_EMIT errorSignal(proc_name, m_pid, maps_file.errorString());
+			}
+			if(mem_file.error() != QFile::NoError)
+			{
+				qCritical("ERROR: Open file error: mem: %s", mem_file.errorString().toLocal8Bit().data());
+				Q_EMIT errorSignal(proc_name, m_pid, mem_file.errorString());
+			}
+			return;
+		}
+		QTextStream maps_str(&maps_file);
+		long unsigned int scanned = 0;
+		qint32 result = CL_CLEAN;
+		const char *virname = NULL;
+		Q_EMIT procScanStartedSignal(proc_name, m_pid);
+		qDebug("INFO: Scanning process: %s(%lli)", proc_name.toLocal8Bit().data(), m_pid);
+		for(QString line = maps_str.readLine(); !line.isNull(); line = maps_str.readLine())
+		{
+			checkPause();
+			if(Scanner::stopped())
+				return;
+			qint32 pos = mem_addr_regex.indexIn(line);
+			if(!pos)
+			{
+				quint64 start = mem_addr_regex.capturedTexts()[1].toLongLong(NULL, 16);
+				quint64 end = mem_addr_regex.capturedTexts()[2].toLongLong(NULL, 16);
+				quint64 size = end - start;
+				for(quint64 read = 0; read < size;)
+				{
+					checkPause();
+					if(Scanner::stopped())
+						return;
+					start += read;
+					mem_file.seek(start);
+					QByteArray buf;
+					buf = mem_file.read(size - read);
+					if((quint64)buf.size() < size - read && mem_file.error() != QFile::NoError)
+					{
+						qCritical("ERROR: Read file error: mem: %s", mem_file.errorString().toLocal8Bit().data());
+// 						Q_EMIT errorSignal(proc_name, m_pid, mem_file.errorString());
+						read += size - read;
+						continue;
+					}
+					
+					cl_fmap_t *fmap = cl_fmap_open_memory(buf.data(), buf.size());
+					result = cl_scanmap_callback(fmap, &virname, &scanned, m_engine, CL_SCAN_STDOPT, NULL);
+					cl_fmap_close(fmap);
+					if(result == CL_VIRUS)
+						break;
+					read += buf.size();
+				}
+				if(result == CL_VIRUS)
+					break;
+			}
+		}
+		Q_EMIT procScanCompletedSignal(proc_name, m_pid, result, virname);
+	}
+
+}
---
+++ sources/clamavqtengine/src/procscanner.h
@@ -0,0 +1,56 @@
+
+/*
+ *  Copyright (C) 2013 Andrey Dudakov
+ *
+ *  Authors: Andrey "amd00" Dudakov
+ *
+ *  This file is part of clamavqtengine.
+ *
+ *  clamavqtengine is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  clamavqtengine is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with clamavqtengine.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _PROCSCANNER_H_
+#define _PROCSCANNER_H_
+
+#include <QProcess>
+
+#include "scanner.h"
+
+class cl_engine;
+
+namespace Meduzzza
+{
+	class ProcScanner : public Scanner
+	{
+		Q_OBJECT
+
+	private:
+		cl_engine *m_engine;
+		Q_PID m_pid;
+
+	public:
+		ProcScanner(cl_engine *_engine, Q_PID &_pid) : Scanner(), m_engine(_engine), m_pid(_pid) {}
+		~ProcScanner() {}
+
+	protected:
+		void run();
+
+	Q_SIGNALS:
+		void procScanStartedSignal(const QString &_name, Q_PID _pid);
+		void procScanCompletedSignal(const QString &_name, Q_PID _pid, qint32 _result, const QString &_virname);
+		void errorSignal(const QString &_name, Q_PID _pid, const QString &_err);
+	};
+}
+
+#endif
